342f9763345a69c6e3ce48d6310f180cb4dc7d75
yasinjodat <yasinjodat1390@gmail.com>
2024-03-26 00:49:26 +0500

    change in app.py

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ import joblib
 app = Flask(__name__)
 model = joblib.load('breast_cancer_model.pkl')
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Split the input string into individual float values
     features = [float(x) for x in request.form['features'].split(',')]
     prediction = model.predict([features])[0]
-    if prediction == 0:
-        result = 'Benign'
-    else:
-        result = 'Malignant'
+    result = 'Benign' if prediction == 0 else 'Malignant'
     return render_template('index.html', prediction_text=f'Predicted Cancer Type: {result}')
 
-if __name__ == '__main__':
+
+if __name__ == '_main_':
     app.run(debug=True)
