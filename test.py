cf00b369c71b8b26944eeaa91765f4c7acace5be
zaema-d <zaema.khurram@gmail.com>
2024-03-27 21:32:38 +0500

    fix unit test for the code

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 
-
 def test_svm_classifier_accuracy():
     # Load dataset and split it
     data = load_breast_cancer()
@@ -16,4 +15,3 @@ def test_svm_classifier_accuracy():
     # Assert if accuracy is greater than 0
     accuracy = clf.score(X_test, y_test)
     assert accuracy > 0
-    
